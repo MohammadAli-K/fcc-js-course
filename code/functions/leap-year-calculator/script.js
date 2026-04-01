2f9a8ebfb8bfe834a162780886857be1884791be
MohammedK <m.ali.kassem@outlook.com>
2026-04-01 11:56:09 +0200

    leap  year challenge af

--- code/functions/leap-year-calculator/script.js
+++ code/functions/leap-year-calculator/script.js
@@ -5,8 +5,22 @@ function isLeapYear(year) {
     return `${year} is not a leap year.`;
   }
 }
-let year = 2024;
+let year = 2045;
 
 let result = isLeapYear(year);
 
 console.log(result);
+
+// Zo kan het ook
+
+// function isLeapYear(year) {
+//   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//     return `${year} is a leap year.`;
+//   } else {
+//     return `${year} is not a leap year.`;
+//   }
+// }
+
+// let result = isLeapYear(2030);
+
+// console.log(result);
